20300c1aad4cc6c34151a0860fda220dc171d6d4
Ananya <90745373+anunicn47@users.noreply.github.com>
2022-10-18 02:03:47 +0000

    💕

---
+++ KuramaRobot/utils/carbon.py
@@ -0,0 +1,11 @@
+
+
+from io import BytesIO
+from KuramaRobot import aiohttpsession
+
+async def make_carbon(code):
+    url = "https://carbonara.vercel.app/api/cook"
+    async with aiohttpsession.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "Cutiepii_Carbon.png"
+    return image
